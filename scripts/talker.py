1eddeb0397470ed25bad458b46822586d652ae2c
Sean Kirmani <sean@kirmani.io>
2016-09-07 15:23:21 -0700

    fixed rostopic publishing to

--- scripts/talker.py
+++ scripts/talker.py
@@ -10,7 +10,7 @@ import rospy
 from geometry_msgs.msg import Twist
 
 def talker():
-  pub = rospy.Publisher('turtlesim', Twist, queue_size=10)
+  pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
   rospy.init_node('talker', anonymous=True)
   rate = rospy.Rate(10)  # 10 Hz
   while not rospy.is_shutdown():
